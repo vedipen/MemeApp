0708ea189dd12865e175a9dd1ef30bb008b7479f
Dipen Ved <vedipen@gmail.com>
2017-03-22 14:46:54 +0530

    Add Project

---
+++ app/app/src/main/java/com/vedipen/myapplication/TopSectionFragment.java
@@ -0,0 +1,22 @@
+package com.vedipen.myapplication;
+
+/**
+ * Created by vedipen on 22/3/17.
+ */
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TopSectionFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.top_section_fragment, container, false);
+        return view;
+    }
+}
+
